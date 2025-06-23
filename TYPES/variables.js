19be426253e1ec58bf27088574216371e6ee69f0
Sarah Hackett <sarah.hackett@flutteruki.com>
2025-06-23 14:11:59 +0100

    Variables and functions

---
+++ TYPES/variables.js
@@ -0,0 +1,9 @@
+// BASICS -- PRIMITIVE TYPES
+// STRINGS
+var movieTitle = "The matrix";
+movieTitle = "The matrix 2";
+movieTitle.toUpperCase();
+// NUMBERS
+var myNum = 10;
+var myBoolean = true;
+myBoolean = false;
---
+++ TYPES/variables.ts
@@ -0,0 +1,22 @@
+// BASICS -- PRIMITIVE TYPES
+
+// STRINGS
+let movieTitle: string = "The matrix";
+movieTitle = "The matrix 2";
+movieTitle.toUpperCase();
+
+// NUMBERS
+let myNum: number = 10;
+let myBoolean: boolean = true;
+myBoolean = false;
+
+// TYPE INFERENCE
+let tvShow = "The office";
+tvShow = "The office 2";
+
+// TYPE ANY
+let anything: any = "hello";
+anything = 10;
+anything = true;
+// extra checks are gone
+anything.toUppercase();
