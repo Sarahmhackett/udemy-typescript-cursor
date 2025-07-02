f7ae69e911a59d69842f21e7bbede4dd400a637f
Sarah Hackett <sarah.hackett@flutteruki.com>
2025-07-02 10:49:29 +0100

    axios and lodash

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,117 @@
-class Player {
-  readonly first: string;
-  readonly last: string;
+import axios from "axios";
+import _ from "lodash";
 
-  protected _score = 0; //infers + can accessed in a child class i.e. SuperPlayer
-  public numLoves: number = 10;
-  private prevScores: number = 0; // it can only be accessed within the class
+// class Player {
+//   readonly first: string;
+//   readonly last: string;
 
-  constructor(first: string, last: string) {
-    this.first = first;
-    this.last = last;
-  }
-  get fullName(): string {
-    return `${this.first} ${this.last}`;
-  }
+//   protected _score = 0; //infers + can accessed in a child class i.e. SuperPlayer
+//   public numLoves: number = 10;
+//   private prevScores: number = 0; // it can only be accessed within the class
 
-  get score(): number {
-    return this._score;
-  }
+//   constructor(first: string, last: string) {
+//     this.first = first;
+//     this.last = last;
+//   }
+//   get fullName(): string {
+//     return `${this.first} ${this.last}`;
+//   }
 
-  set score(newScore: number) {
-    this.score = newScore;
-  }
-}
+//   get score(): number {
+//     return this._score;
+//   }
 
-const playerOne = new Player("Sarah", "Hackett");
+//   set score(newScore: number) {
+//     this.score = newScore;
+//   }
+// }
 
-// SHORTCUTS
-class ShortcutPlayer {
-  constructor(public first: string, public last: string) {}
-}
+// const playerOne = new Player("Sarah", "Hackett");
 
-const shortcutPlayerOne = new ShortcutPlayer("Lazy", "Player");
+// // SHORTCUTS
+// class ShortcutPlayer {
+//   constructor(public first: string, public last: string) {}
+// }
 
-class SuperPlayer extends Player {
-  public isAdmin: boolean = true;
-  maxScore() {
-    this._score = 1000000;
-  }
-}
+// const shortcutPlayerOne = new ShortcutPlayer("Lazy", "Player");
 
-// INTERFACES
-interface Visitor {
+// class SuperPlayer extends Player {
+//   public isAdmin: boolean = true;
+//   maxScore() {
+//     this._score = 1000000;
+//   }
+// }
+
+// // INTERFACES
+// interface Visitor {
+//   name: string;
+// }
+
+// class Watcher implements Visitor {
+//   constructor(public name: string) {}
+// }
+
+// const anonWatcher = new Watcher("David");
+
+///// AXIOS /////
+
+// https://jsonplaceholder.typicode.com/users
+
+///// AXIOS ///////
+
+interface AnotherUser {
+  id: string;
   name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
 }
 
-class Watcher implements Visitor {
-  constructor(public name: string) {}
+// Single User end point request. Uses the Another User interface to define the structure of the response.
+axios
+  .get<AnotherUser>("https://jsonplaceholder.typicode.com/users/1")
+  .then((res) => {
+    console.log("Successfully fetched users");
+    const { data } = res;
+    data;
+
+    printAnotherUser(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+/// Multiple users. Here we tell Axios we're expecting an array of AnotherUser objects to come back.
+axios
+  .get<AnotherUser[]>("https://jsonplaceholder.typicode.com/users")
+  .then((res) => {
+    console.log("Successfully fetched users");
+    res.data.forEach(printAnotherUser);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+function printAnotherUser(user: AnotherUser) {
+  console.log(user.name);
 }
 
-const anonWatcher = new Watcher("David");
+////// LODASH - DOESN'T HAVE TYPESCRIPT TYPE DEFINITIONS - NEED TO INSTALL SEPERATELY - DEFINITELYTYPED VIA @TYPES/ /////
+
+_.sample([1, 2, 3, 4, 5]);
